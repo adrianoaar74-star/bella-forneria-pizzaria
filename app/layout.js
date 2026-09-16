72c9a9bb1d5050004ad566931be1bdcc9a2900ae
adrianoaar74-star <adriano.aar74@gmail.com>
2026-09-16 16:01:26 -0300

    Adiciona layout principal

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Bella Forneria Pizzaria | Mogi Guaçu",
+  description: "Tradição italiana, preparo artesanal e sabor de verdade em Mogi Guaçu.",
+};
+
+export default function RootLayout({ children }) {
+  return <html lang="pt-BR"><body>{children}</body></html>;
+}
